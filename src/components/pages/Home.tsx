a9aed59e00a0049a6fedbf13d7a99b4e0e7f1885
Vinicius Bohrer <vbsanttos@gmail.com>
2021-09-12 02:15:29 -0300

    fix

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -120,7 +120,7 @@ const Home = () => (
           </div>
         </div>
         <div>
-          <video src={AboutVideo} width="100%" autoPlay loop />
+          <video src={AboutVideo} width="100%" autoPlay muted loop />
         </div>
       </Grid>
     </Section>
